844560896621ba6d0457eb5792f2222f5403a62f
Max Nikulin <m.nikulin@movavi.com>
2019-05-11 16:48:16 +0700

    Add initial Xcode project

---
+++ Lamptest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Lamptest
+//
+//  Created by Max Nikulin on 11/05/2019.
+//  Copyright © 2019 Lamptest. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Lamptest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lamptest
+//
+//  Created by Max Nikulin on 11/05/2019.
+//  Copyright © 2019 Lamptest. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
